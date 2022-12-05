6e00e14866e1ee7ca8cd9b86f700185c679053fd
rococo <xiconz@qq.com>
2022-12-05 16:49:08 +0800

    chore: :construction_worker: 添加git comiit提交时的校验

---
+++ .commitlintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+	'extends': ['@commitlint/config-conventional'],
+	'type-enum': [
+		2,
+		'always',
+		[
+			'feat',
+			'fix',
+			'perf',
+			'style',
+			'docs',
+			'test',
+			'refactor',
+			'build',
+			'ci',
+			'chore',
+			'revert',
+			'wip',
+			'workflow',
+			'types',
+			'release'],
+	],
+}
