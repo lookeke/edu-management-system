5cc71cbe7b3c65b13294a930d31ae6c0b7d05b8a
rococo <xiconz@qq.com>
2022-12-05 17:31:47 +0800

    feat: :label: 添加ts类型支持

---
+++ client/src/types/index.d.ts
@@ -0,0 +1,14 @@
+
+import { ReactNode } from 'react'
+
+// 路由
+export type IRouter = {
+	path: string
+	element: ReactNode
+}[]
+
+// 导航路由
+export type INavRouter = {
+	path: string
+	label: string
+}[]
