f2dea257c2a42258e38db99c5c4e6b71e9f2ce96
rococo <xiconz@qq.com>
2022-12-08 13:19:49 +0800

    feat: 添加路由状态管理

---
+++ client/src/features/router/index.ts
@@ -0,0 +1,34 @@
+import type { PayloadAction } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
+
+export type Route = {
+	router: {
+		items: {
+			path: string
+			label: string
+		}[]
+		role: number | null
+	}
+}
+
+const initialState: { value: Route } = {
+	value: {
+		router: {
+			items: [],
+			role: null
+		},
+	}
+}
+
+export const routerSlice = createSlice({
+	name: 'router',
+	initialState,
+	reducers: {
+		createRouter: (state, { payload }: PayloadAction<Route>) => {
+			state.value.router.items = [...payload.router.items]
+		},
+	},
+})
+
+export const { createRouter } = routerSlice.actions
+export default routerSlice.reducer
